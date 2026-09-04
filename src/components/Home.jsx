1db67313750a944362ccd39727f36b1c52a15eb4
Ladali Kumari <ladalikumari683@gmail.com>
2026-09-04 17:49:17 +0530

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -121,4 +121,4 @@ export default function PsychiatristHero() {
       </section>
     </div>
   );
-}
+}                                                                                                                                                                        
